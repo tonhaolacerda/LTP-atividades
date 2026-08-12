5a9cba11df41827d56399a4de97a50e61e39079c
Heloisa Nunes <helo.nunesfer@gmail.com>
2026-08-12 11:37:19 -0400

    atividadeprofessor

--- atividadeprofessor.js
+++ atividadeprofessor.js
@@ -11,5 +11,22 @@ class Funcionario {
 }
 
 class Professor extends Funcionario {
-    
+    constructor(nome,salario,disciplina){
+        super(nome,salario);
+        this.disciplina = disciplina;
+    }
+
+    darAula(Professor) {
+        return(`O professor ${this.nome} está ministrando a disciplina de Programacao`)
+    }
+
 }
+
+const professor1 = new Professor("Anthony", 20, "Programacao")
+
+console.log(professor1.nome);
+console.log(professor1.disciplina);
+console.log(professor1.salario);
+
+professor1.trabalhar();
+professor1.darAula();
